85f1f8397dab896f9ffc96389e31b3e2ea1d8693
Colton Crouch <colton.crouch96@gmail.com>
2017-12-21 22:36:16 -0500

    remove complete game, finish up to part 2 of Space Shooter

---
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary {
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+    public Boundary boundary;
+    public float speed;
+    public float tilt;
+
+    public GameObject bolt;
+    public Transform boltSpawn;
+    public float fireDelta;
+
+    private float nextFire;
+
+    private Rigidbody rb;
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        if (Input.GetButton("Fire1") && Time.time > nextFire) {
+            nextFire = Time.time + fireDelta;
+            Instantiate(bolt, boltSpawn.position, boltSpawn.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector3(
+            x: Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            y: 0.0f,
+            z: Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+    }
+}
